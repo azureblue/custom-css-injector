f9178c943f5dacf1ea67da76d249160632fe8c73
Konrad <kk0xf0@gmail.com>
2024-11-06 23:38:24 +0100

    more work

--- content-script.js
+++ content-script.js
@@ -1,24 +1,19 @@
 (async () => {
+    const styleElementId = '__custom-css-injector';  
     const hostname = window.location.hostname;
     const entry = await chrome.storage.sync.get(hostname);
     
-    let css = (entry[hostname] != undefined) ? entry[hostname] : "";
-    css = css.trim();
+    let css = (entry[hostname] || "").trim();
     if (css.length == 0) 
       return;
 
-    let style = document.getElementById('__custom-css-style');
+    let style = document.getElementById(styleElementId);
+
     if (style == undefined) {
-      head = document.head || document.getElementsByTagName('head')[0],          
       style = document.createElement('style');
-      style.id = '__custom-css-style';
-      head.appendChild(style);
+      style.id = styleElementId;
+      document.head.appendChild(style);
     }
-     
-    style.type = 'text/css';
-    if (style.styleSheet){            
-      style.styleSheet.cssText = css;
-    } else {
-      style.appendChild(document.createTextNode(css));
-    }
+
+    style.replaceChildren(document.createTextNode(css));
 })();
--- popup.js
+++ popup.js
@@ -1,18 +1,16 @@
 /**@type {HTMLTextAreaElement} */
 let textArea = document.getElementById("style-text");
-
 let saveButton = document.getElementById("save");
-async function load() {
+
+(async () => {
   const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-  const hostname = new URL(tab.url).hostname;
+  const hostname = new URL(tab.url).hostname;  
   const entry = await chrome.storage.sync.get(hostname);
-  if (entry[hostname] != undefined)
-    textArea.value = entry[hostname];
-}
-load();
+  textArea.value = entry[hostname] || "";  
+})();
 
 saveButton.addEventListener('click', async () => {
   const [tab] = await chrome.tabs.query({ active: true, currentWindow: true })
   const hostname = new URL(tab.url).hostname;    
-  chrome.storage.sync.set({[hostname] : textArea.value});
+  chrome.storage.sync.set({[hostname] : textArea.value.trim()});
 });
