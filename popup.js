1f52b56f04843374ac74799fb3fdb2a56720dd69
Konrad <kk0xf0@gmail.com>
2024-11-09 14:10:49 +0100

    change popup

--- popup.js
+++ popup.js
@@ -1,12 +1,15 @@
 /**@type {HTMLTextAreaElement} */
 let textArea = document.getElementById("style-text");
+/**@type {HTMLLabelElement} */
+let label = document.getElementById("style-label");
 let saveButton = document.getElementById("save");
 
 (async () => {
   const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
   const hostname = new URL(tab.url).hostname;  
   const entry = await chrome.storage.sync.get(hostname);
-  textArea.value = entry[hostname] || "";  
+  textArea.value = entry[hostname] || "";
+  label.textContent = `Custom CSS for ${hostname}:`
 })();
 
 saveButton.addEventListener('click', async () => {
